a8b85be68584c4a791b1940f922e07bb6b9ceebc
Stella McEwen <mcewen.stella@gmail.com>
2019-05-21 20:06:41 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,41 @@
-
-
+class Book 
+  def initialize(title)
+    @title = title
+  end
+  
+  def title 
+    @title
+  end 
+  
+  def title=(book_title) 
+    @title = book_title
+  end
+  
+  def author 
+    @author 
+  end 
+  
+  def author=(book_author)
+    @author = book_author
+  end
+  
+  def page_count 
+    @page_count 
+  end 
+  
+  def page_count=(page_count)
+    @page_count = page_count 
+  end 
+  
+  def genre
+    @genre 
+  end 
+  
+  def genre=(genre)
+    @genre = genre
+  end
+  
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+end 
